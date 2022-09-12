ab75df03b88ffc15f463c7267a94f4b976ec89b9
“lindsayvm” <lindsay.leek@hotmail.com>
2022-09-12 14:27:03 +0200

    mrr.txt also need point annotation field, and some minor changes in how to read data ==> DOES FULLY RUN NOW

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = {'Answer': [0, 1, 2, 3], 'Question': ["tumor","tumor","lymph","lymph"]}
-df = pd.DataFrame(data=df, index=[0, 1, 2, 3])
-
-print(df.loc[df['Question'] =="tumor"])
-#for _, p in self.scores[self.scores['Question'] == boxes_question].iterrows()]
