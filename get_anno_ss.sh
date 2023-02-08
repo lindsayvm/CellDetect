60597ce5ac73cddbf4d5d9a165506e4fd1e39888
“lindsayvm” <lindsay.leek@hotmail.com>
2023-02-08 17:00:45 +0100

    retreive anno from slidescore in json

---
+++ get_anno_ss.sh
@@ -0,0 +1,6 @@
+slidescore -s 1844 download-labels -q Lymphocytes -u l.leek@nki.nl -o 'GEOJSON' lymph
+
+slidescore -s 1844 download-labels -q Tumorcells -u l.leek@nki.nl -o 'GEOJSON' tumor
+
+
+#slidescore -s 1844 upload-labels-from-geojson -u l.leek@nki.nl -i 386687 -n 'T19-07593 I5 HE', -g file1.json 
